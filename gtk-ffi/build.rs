af928da2d9beb05145039d958d2161d7bdd10772
Michael Aaron Murphy <mmstick@pm.me>
2022-11-04 23:39:05 +0100

    chore: Make builds compatible with shared target-dir path

--- gtk-ffi/build.rs
+++ gtk-ffi/build.rs
@@ -1,9 +1,17 @@
-use std::{env, fs::File, io::Write, path::PathBuf};
+use std::{env, fs::File, io::Write, path::PathBuf, os::unix::prelude::OsStrExt};
 
 fn main() {
     cdylib_link_lines::metabuild();
 
-    let target_dir = PathBuf::from("../target");
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let mut target_dir = out_dir.as_path();
+
+    while let Some(parent) = target_dir.parent() {
+        if target_dir.as_os_str().as_bytes().ends_with(b"target") {
+            break
+        }
+        target_dir = parent;
+    }
 
     let pkg_config = format!(
         include_str!("pop_system_updater_gtk.pc.in"),
@@ -12,7 +20,9 @@ fn main() {
         version = env::var("CARGO_PKG_VERSION").unwrap()
     );
 
-    File::create(target_dir.join("pop_system_updater_gtk.pc.stub"))
+    std::fs::create_dir_all("../target").unwrap();
+
+    File::create("../target/pop_system_updater_gtk.pc.stub")
         .expect("failed to create pc.stub")
         .write_all(pkg_config.as_bytes())
         .expect("failed to write pc.stub");
